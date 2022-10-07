a5d66f401bcda62fef2be48c597722a0f14f34aa
fabio cano <fabio.cano@mercadolibre.com.co>
2022-10-07 17:55:01 -0500

    Add tests for tunr on all lights

--- kata-log/christmas_lights.go
+++ kata-log/christmas_lights.go
@@ -1,5 +1,15 @@
 package kata_log
 
-func Display(lights [][]string) [][]string {
+func Display(lights [][]string, turnOn bool) [][]string {
+	if turnOn {
+		for i := range lights {
+			lightRow := lights[i]
+			for f := range lightRow {
+				lightRow[f] = "1"
+			}
+			lights[i] = lightRow
+		}
+		return lights
+	}
 	return lights
 }
--- kata-log/christmas_lights_test.go
+++ kata-log/christmas_lights_test.go
@@ -6,27 +6,57 @@ import (
 )
 
 func TestDisplay(t *testing.T) {
-	want := make([][]string, 1000)
 
-	want = getLightsAllOff(want)
-	got := Display(want)
+	t.Run("Should display all lights on off", func(t *testing.T) {
+		want := make([][]string, 1000)
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
+		want = getLightsAllOff(want)
+		got := Display(want, false)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("Should display all lights on on", func(t *testing.T) {
+		want := make([][]string, 1000)
+		lights := make([][]string, 1000)
+
+		want = getLightsAllOn(want)
+		got := Display(getLightsAllOff(lights), true)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func getLightsAllOn(want [][]string) [][]string {
+	for i := range want {
+		want[i] = getLightRowAllOn()
 	}
+	return want
+}
+
+func getLightRowAllOn() []string {
+	lightRow := make([]string, 1000)
+	for i := range lightRow {
+		lightRow[i] = "1"
+	}
+	return lightRow
 }
 
 func getLightsAllOff(want [][]string) [][]string {
-	for range want {
-		want = append(want, getLightRowAllOff())
+	for i := range want {
+		want[i] = getLightRowAllOff()
 	}
 	return want
 }
 
 func getLightRowAllOff() []string {
 	lightRow := make([]string, 1000)
-	for range lightRow {
-		lightRow = append(lightRow, "0")
+	for i := range lightRow {
+		lightRow[i] = "0"
 	}
 	return lightRow
 }
